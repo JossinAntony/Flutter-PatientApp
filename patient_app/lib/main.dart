9a6884e887542a62f71217c95547e96109424754
JossinAntony <jossinde@gmail.com>
2020-08-15 16:19:05 +0200

    ui- initial text field defined.

--- patient_app/lib/main.dart
+++ patient_app/lib/main.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
@@ -13,6 +14,31 @@ class MyApp extends StatelessWidget {
           title: Text('Patient App', style: TextStyle(
             fontWeight: FontWeight.bold,
           ),),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+
+            padding: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter patient name",
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                SizedBox(height:20.0),
+                RaisedButton(
+                  onPressed: (){},
+                  child: Text('SAVE'),
+                  color: Colors.blueAccent,
+                ),
+              ],
+            ),
+          ),
         ),
       )
     );
