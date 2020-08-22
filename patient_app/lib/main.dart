0a5415d255c1d788015ff67d2149b7c65466723c
JossinAntony <jossinde@gmail.com>
2020-08-22 07:50:34 +0200

    Input displayed on console Custom button design initialised

--- patient_app/lib/main.dart
+++ patient_app/lib/main.dart
@@ -1,12 +1,21 @@
 
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  TextEditingController pNameCntrlr = TextEditingController();
+  TextEditingController pAdmnCntrlr = TextEditingController();
+  TextEditingController pAdrssCntrlr = TextEditingController();
+
+  TextEditingController pMobCntrlr = TextEditingController();
+  TextEditingController pEmailCntrlr = TextEditingController();
+  TextEditingController pBGrpCntrlr = TextEditingController();
+
+  TextEditingController pInsCntrlr = TextEditingController();
+  TextEditingController pDocCntrlr = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,6 +34,7 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 TextField(
+                  controller: pNameCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter patient name",
@@ -35,6 +45,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height:20.0),
 
                 TextField(
+                  controller: pAdmnCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter patient admission no.",
@@ -45,6 +56,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height:20),
 
                 TextField(
+                  controller: pAdrssCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter the address",
@@ -55,6 +67,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height:20.0),
 
                 TextField(
+                  controller: pMobCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter the mobile no. of patient",
@@ -65,6 +78,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height:20.0),
 
                 TextField(
+                  controller: pEmailCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter the email address of the patient",
@@ -75,6 +89,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height:20.0),
 
                 TextField(
+                  controller: pBGrpCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter the blood group of the patient",
@@ -85,6 +100,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height:20.0),
 
                 TextField(
+                  controller: pInsCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter the insurance number of the patient",
@@ -95,6 +111,7 @@ class MyApp extends StatelessWidget {
                 SizedBox(height:20.0),
 
                 TextField(
+                  controller: pDocCntrlr,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter the consulting doctor",
@@ -103,11 +120,43 @@ class MyApp extends StatelessWidget {
                 ),
 
                 SizedBox(height:20.0),
-
+                /*
                 RaisedButton(
-                  onPressed: (){},
+                  onPressed: (){
+                    var pName = pNameCntrlr.text;
+                    var pAdrss = pAdrssCntrlr.text;
+                    var pMob = pMobCntrlr.text;
+
+                    var pEmail = pEmailCntrlr.text;
+                    var pBGrp = pBGrpCntrlr.text;
+                    var pIns = pInsCntrlr.text;
+
+                    var pDoc = pDocCntrlr.text;
+
+                    print(pName);
+                    print(pAdrss);
+                    print(pMob);
+
+                    print(pEmail);
+                    print(pBGrp);
+                    print(pIns);
+
+                    print(pDoc);
+                  },
                   child: Text('SAVE'),
                   color: Colors.blueAccent,
+                ),
+                */
+                Container(
+                  height: 60,
+                  width: 500,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.blueAccent,
+                  ),
+                  child: Center(
+                    child: Text('SAVE'),
+                  ),
                 ),
               ],
             ),
