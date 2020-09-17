a0dfaaca08628553e6909b7614fde4bf65138876
JossinAntony <jossinde@gmail.com>
2020-09-17 12:33:38 +0200

    SearchPatient.dart added

--- patient_app/lib/main.dart
+++ patient_app/lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:patient_app/pages/SearchPatient.dart';
 import 'package:patient_app/pages/login.dart';
 
 main() => runApp(MyApp());
@@ -9,7 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LogIn(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Patient App'),
+        ),
+        body: SearchPatient(),
+      ),
     );
   }
 }
--- patient_app/lib/pages/SearchPatient.dart
+++ patient_app/lib/pages/SearchPatient.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 
 class SearchPatient extends StatelessWidget {
+
+  TextEditingController patientNameCntrlr = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return ListView(
         children: [
           Expanded(
             child: Container(
+              margin: EdgeInsets.all(20),
               child: Column(
                 children: <Widget>[
                   TextField(
+                    controller: patientNameCntrlr,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular((12)),
                       ),
                       prefixIcon: Icon(Icons.search),
                       hintText: 'Enter patient\'s name to search',
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+
+                  ButtonTheme(
+                    minWidth: 200,
+                    height: 50,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: RaisedButton(
+                      onPressed: (){
+
+                      },
+                      child: Text('SUBMIT'),
                     ),
                   ),
                 ],
