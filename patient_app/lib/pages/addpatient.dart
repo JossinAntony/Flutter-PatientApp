1f030bd4e4a4f41b112b74e7b20340e4d6ee67a6
JossinAntony <jossinde@gmail.com>
2020-10-04 08:15:40 +0200

    datamodel in addpatient.dart.

--- patient_app/lib/pages/addpatient.dart
+++ patient_app/lib/pages/addpatient.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:patient_app/datamodel/patientmodel.dart';
 
 class AddPatient extends StatelessWidget {
   TextEditingController pNameCntrlr = TextEditingController();
@@ -103,49 +104,22 @@ class AddPatient extends StatelessWidget {
               SizedBox(height: 20.0),
 
               /*
-
                   RaisedButton(
-
                     onPressed: (){
-
                       var pName = pNameCntrlr.text;
-
                       var pAdrss = pAdrssCntrlr.text;
-
                       var pMob = pMobCntrlr.text;
-
-
-
                       var pEmail = pEmailCntrlr.text;
-
                       var pBGrp = pBGrpCntrlr.text;
-
                       var pIns = pInsCntrlr.text;
-
-
-
                       var pDoc = pDocCntrlr.text;
-
-
-
                       print(pName);
-
                       print(pAdrss);
-
                       print(pMob);
-
-
-
                       print(pEmail);
-
                       print(pBGrp);
-
                       print(pIns);
-
-
-
                       print(pDoc);
-
                     },
 
                     child: Text('SAVE'),
@@ -156,15 +130,22 @@ class AddPatient extends StatelessWidget {
 
                   */
 
-              Container(
-                height: 40,
-                width: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12.0),
-                  color: Colors.blueAccent,
-                ),
-                child: Center(
-                  child: Text('SAVE'),
+              GestureDetector(
+                onTap: (){
+                  var patient = PatientModel(patientName: pNameCntrlr.text, address:pAdrssCntrlr.text,admnno: pAdmnCntrlr.text, mobileno: pMobCntrlr.text,
+                  bloodgrp: pBGrpCntrlr.text, consultingDr: pDocCntrlr.text, emailId: pEmailCntrlr.text, insuranceno: pInsCntrlr.text);
+                  print(patient.patientName);
+                },
+                child: Container(
+                  height: 40,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.blueAccent,
+                  ),
+                  child: Center(
+                    child: Text('SAVE'),
+                  ),
                 ),
               ),
               SizedBox(height:50),
